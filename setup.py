e7a52f842cef2a7c2ad54a1fb4a0e22dc4a3aaf2
Juan Rojas <juananrojas1997@gmail.com>
2023-03-22 23:45:27 -0500

    Fix error in setup.py

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ setuptools.setup(
             'Programming Language :: Python :: 3',
             'Topic :: Scientific/Engineering',
             'Topic :: Software Development :: Libraries',
-            'Topic :: Software Development :: Libraries :: Python Modules'
-            'Topic :: Software Development :: Pre-processors',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: Pre-processors'
         ],
     python_requires='>=3.9',
     install_requires=["pandas","typeguard"],
